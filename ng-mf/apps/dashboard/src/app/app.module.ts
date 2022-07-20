2bb7072ad695f050b96ad7d8e6b7f792c8d25b07
Edward Endovickiy <greeny0909@gmail.com>
2022-07-20 19:21:25 +0300

    Fix host + add routes

--- ng-mf/apps/dashboard/src/app/app.module.ts
+++ ng-mf/apps/dashboard/src/app/app.module.ts
@@ -3,7 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginGuard } from './login.guard';
 import { WrapperComponent } from './wrapper/wrapper.component';
+
+enum REMOTE_URL {
+  PERSONS = 'persons',
+  LOGIN = 'login'
+}
 
 @NgModule({
   declarations: [AppComponent, WrapperComponent],
@@ -13,20 +19,19 @@ import { WrapperComponent } from './wrapper/wrapper.component';
       [
         {
           path: '',
+          canActivate: [LoginGuard],
           component: WrapperComponent,
-          canActivate:
+          children: [
+            {
+              path: REMOTE_URL.PERSONS,
+              loadChildren: () => import('persons/Module').then((m) => m.RemoteEntryModule),
+            }
+          ]
         },
         {
-          path: 'login',
-          loadChildren: () =>
-            import('login/Module').then((m) => m.RemoteEntryModule),
-        },{
-         path: 'items', 
-         loadChildren: () => import('items/Module').then(m => m.RemoteEntryModule)
-     }, {
-         path: 'persons', 
-         loadChildren: () => import('persons/Module').then(m => m.RemoteEntryModule)
-     }
+          path: REMOTE_URL.LOGIN,
+          loadChildren: () => import('login/Module').then((m) => m.RemoteEntryModule),
+        },
       ],
       { initialNavigation: 'enabledBlocking' }
     ),
